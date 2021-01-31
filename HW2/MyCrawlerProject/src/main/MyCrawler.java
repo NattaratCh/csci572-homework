2362d6c344a1b499c3ddac3d12daa38a978ea79f
Nattarat Ch <champree@usc.edu>
2021-01-30 20:55:54 -0800

    Add new csv file results

--- HW2/MyCrawlerProject/src/main/MyCrawler.java
+++ HW2/MyCrawlerProject/src/main/MyCrawler.java
@@ -17,9 +17,19 @@ import org.slf4j.LoggerFactory;
 public class MyCrawler extends WebCrawler {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MyCrawler.class);
 
-	private static final Pattern FILTERS = Pattern.compile(".*(\\.(css|js|script|mp3|mp4|zip|gz|rss)($|\\?)).*");
+	private static final Pattern FILTERS = Pattern.compile(
+			".*(\\.(css|js|bmp|tiff?|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v" +
+					"|rm|smil|wmv|swf|wma|zip|rar|gz|jnlp|woff|ttf|xls|xlsx|ppt|pptx|json|xml))$");
 	private static final Pattern WHITE_LIST = Pattern.compile(".*(\\.(html|HTML|doc|pdf|jpg|jpeg|png|gif))$");
-	private static final Set<String> FILTERS_CONTENT_TYPE = new HashSet(Arrays.asList("text/css", "text/javascript", "application/json"));
+	private static final Set<String> FILTERS_CONTENT_TYPE = new HashSet(Arrays.asList(
+			"text/css",
+			"text/javascript",
+			"application/json",
+			"application/json+oembed",
+			"application/javascript",
+			"text/xml",
+			"application/xml"
+	));
 	private static final String[] insideUrls = {
 			"https://www.nytimes.com",
 			"http://www.nytimes.com",
@@ -98,11 +108,11 @@ public class MyCrawler extends WebCrawler {
 
 	private PointerStatus getPointerStatus(String url) {
 		url = url.toLowerCase();
-		if (!FILTERS.matcher(url).matches()
-				|| url.startsWith(insideUrls[0])
+		if (!FILTERS.matcher(url).matches() && (
+				url.startsWith(insideUrls[0])
 				|| url.startsWith(insideUrls[1])
 				|| url.startsWith(insideUrls[2])
-				|| url.startsWith(insideUrls[3]))
+				|| url.startsWith(insideUrls[3])))
 			return PointerStatus.OK;
 		else
 			return PointerStatus.N_OK;
